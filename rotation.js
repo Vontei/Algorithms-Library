34eb95ed7e55db650df50a8568882a39ce0264c5
Nick Mattei <nmattei89@gmail.com>
2015-08-13 11:01:57 -0600

    almost done with rotation

---
+++ rotation.js
@@ -0,0 +1,24 @@
+var x = 'hello'
+var y = 'llohe'
+
+var result = []
+var s1 = x.split('')
+var s2 = y.split('')
+var z = y.indexOf(x[0])
+
+
+for(i=0; i<s1.length; i++){
+    for(j=0;j<s2.length; j++){
+        if(s2[j]>=z && s2[j]<=s1.length-1)
+          if(s1[i]===s2[j+z]){
+            result.push(s1[i])
+          }
+        if(s2[j]<=z && s2[j]>=0){
+            if(s1[i]===s2[j-(z-1)])
+            result.push(s1[i])
+        }
+
+    }
+}
+
+console.log(result)
