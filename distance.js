4d1290bca22b820c540ec01239c2aa3fb2f5fd7c
Nick Mattei <nmattei89@gmail.com>
2015-08-31 09:29:27 -0600

    distance formula

---
+++ distance.js
@@ -0,0 +1,26 @@
+///simply implementing the distance formula
+
+var distance = function(x1,y1,x2,y2){
+    var start = Math.pow((x1-y1),2)
+    var finish = Math.pow((x2-y2),2)
+    var result = start + finish
+    var answer = Math.sqrt(result)
+    return answer
+}
+
+distance(3,12,8,0)
+
+//answer is roughly 13
+
+
+///or
+
+
+function Coordinates(x,y){
+  this.x=x;
+  this.y=y;
+}
+
+function findDistance(node1,node2){
+  return Math.hypot(node2.x-node1.x, node2.y-node1.y)
+}
