c1ab9f0822a201977821e3eb635009553e060219
Nick Mattei <nmattei89@gmail.com>
2015-09-04 10:06:48 -0600

    comments

--- secondPrime.js
+++ secondPrime.js
@@ -1,6 +1,8 @@
 // * An integer is prime if it is not divisible by any prime less than or equal to its square root
 // 2      3      5      7     11     13     17     19     23     29
 //    31     37     41     43     47     53     59     61     67     71
+//return the second prime after the input
+
 function isPrime(num){
   var isPrime = true
   if(num<2){
